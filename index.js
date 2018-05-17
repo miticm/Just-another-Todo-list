5633fad27d95110a2fa6dd7a8284691224ac9122
Tianchi Xu <xtc459924276@gmail.com>
2018-05-17 16:10:53 -0400

    add button group

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
 const app = {
-    init(selector) {
-        this.flicks = []
-        this.max = 0
-        this.list = document.querySelector(selector.listSelector)
-
-        document.querySelector(selector.formSelector).addEventListener('submit', ev => {
-            ev.preventDefault()
-            this.handleSubmit(ev)
+    init(selectors) {
+      this.flicks = []
+      this.max = 0
+      this.list = document.querySelector(selectors.listSelector)
+      this.template = document.querySelector(selectors.templateSelector)
+  
+      document
+        .querySelector(selectors.formSelector)
+        .addEventListener('submit', ev => {
+          ev.preventDefault()
+          this.handleSubmit(ev)
         })
     },
-    
-    renderListItem(flick){
-        const item = document.createElement('li')
-        item.dataset.id = flick.id
-        item.textContent = flick.name
-        return item
+  
+    renderListItem(flick) {
+      const item = this.template.cloneNode(true)
+      item.classList.remove('template')
+      item.dataset.id = flick.id
+      item
+        .querySelector('.flickName')
+        .textContent = flick.name
+  
+      return item
     },
-
+  
     handleSubmit(ev) {
-        const f = ev.target
-        const flick = {
-            id: ++this.max, //this bind to the event target if not using arrow function
-            name: f.flickName.value,
-        }
-
-        this.flicks.unshift(flick)
-        
-        const item = this.renderListItem(flick)
-        this.list.insertBefore(item,this.list.firstChild)
-        f.reset()
+      const f = ev.target
+      const flick = {
+        id: ++this.max,
+        name: f.flickName.value,
+      }
+  
+      this.flicks.unshift(flick)
+  
+      const item = this.renderListItem(flick)
+      this.list.insertBefore(item, this.list.firstChild)
+  
+      f.reset()
     },
-}
-
-app.init({
+  }
+  
+  app.init({
     formSelector: '#flickForm',
     listSelector: '#flickList',
-})
+    templateSelector: '.flick.template',
+  })
