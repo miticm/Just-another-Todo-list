666ab446530b7edea64427da6f2ca14f466d7ae2
Tianchi Xu <xtc459924276@gmail.com>
2018-05-18 00:11:24 -0400

    All five buttons work now

--- index.js
+++ index.js
@@ -20,13 +20,34 @@ const app = {
       item
         .querySelector('.flickName')
         .textContent = flick.name
+
       item.querySelector("#deleteBtn").addEventListener('click',()=>{
         item.parentNode.removeChild(item)
       })
+
       item.querySelector('#editBtn').addEventListener('click',()=>{
         const textSpan = item.querySelector('.flickName')
         textSpan.contentEditable = 'true'
         textSpan.focus()
+      })
+
+      item.querySelector('#favBtn').addEventListener('click',()=>{
+        flick.fav = !flick.fav
+        flick.fav 
+          ? item.querySelector('.fa-star').style.color = '#CD0000' 
+          : item.querySelector('.fa-star').style.color = 'white'
+      })
+
+      item.querySelector('#upBtn').addEventListener('click',()=>{
+        item.parentNode.removeChild(item)
+        this.list.insertBefore(item, this.list.firstChild)
+        this.buttonDisabled()
+      })
+      
+      item.querySelector('#downBtn').addEventListener('click',()=>{
+        item.parentNode.removeChild(item)
+        this.list.appendChild(item)
+        this.buttonDisabled()
       })
   
       return item
@@ -37,16 +58,24 @@ const app = {
       const flick = {
         id: ++this.max,
         name: f.flickName.value,
+        fav: false,
       }
-  
       this.flicks.unshift(flick)
-  
       const item = this.renderListItem(flick)
       this.list.insertBefore(item, this.list.firstChild)
-  
+      this.buttonDisabled()
       f.reset()
     },
+    buttonDisabled(){
+      this.list.childNodes.forEach(element => {
+        element.querySelector('#upBtn').disabled = false
+        element.querySelector('#downBtn').disabled = false
+      });
+      this.list.firstChild.querySelector('#upBtn').disabled = true;
+      this.list.lastChild.querySelector('#downBtn').disabled = true;
+    }
   }
+
   
   app.init({
     formSelector: '#flickForm',
